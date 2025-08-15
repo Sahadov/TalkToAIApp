c5e6137ce22777bcf5b9d61e19e461a685e74ace
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-08-15 15:17:15 -0500

    Add ProfileView

---
+++ TalkToAIChat/Core/Profile/ProfileView.swift
@@ -0,0 +1,44 @@
+//
+//  ProfileView.swift
+//  TalkToAIChat
+//
+//  Created by Dmitry Volkov on 15/08/2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    @State private var showSettingsView: Bool = false
+    
+    var body: some View {
+        NavigationStack {
+            Text("Profile")
+                .navigationTitle("Profile")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        settingsButton
+                    }
+                }
+        }
+        .sheet(isPresented: $showSettingsView) {
+            Text("SettingsView")
+        }
+    }
+    
+    private var settingsButton: some View {
+        Button {
+            onSettingsButtonPressed()
+        } label: {
+            Image(systemName: "gear")
+                .font(.headline)
+        }
+    }
+    
+    private func onSettingsButtonPressed() {
+        showSettingsView = true
+    }
+}
+
+#Preview {
+    ProfileView()
+}
