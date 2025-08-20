1b2fbc6becc9505d7c7cf77f27d1ddd633aaed5d
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-08-19 22:35:22 -0500

    Add OnboardingIntroView

---
+++ TalkToAIChat/Core/Onboarding/ColorView/OnboardingColorView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingColorView.swift
+//  TalkToAIChat
+//
+//  Created by Dmitry Volkov on 19/08/2025.
+//
+
+import SwiftUI
+
+struct OnboardingColorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingColorView()
+}
