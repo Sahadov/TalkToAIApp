ad9764289bbad26ce960a4aad867558a6831d843
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-08-19 19:31:30 -0500

    Refactor AppView

--- TalkToAIChat/Core/Root/AppView.swift
+++ TalkToAIChat/Core/Root/AppView.swift
@@ -8,17 +8,26 @@
 import SwiftUI
 
 struct AppView: View {
-    @AppStorage("showTabbarView") var showTabBar: Bool = false
-    
+
+    @State var appState: AppState = AppState()
+
     var body: some View {
         AppViewBuilder(
-            showTabBar: showTabBar,
+            showTabBar: appState.showTabBar,
             tabbarView: {
                 TabBarView()
             },
             onboardingView: {
-                TabBarView()
+                WelcomeView()
             }
         )
+        .environment(appState)
     }
+}
+
+#Preview("AppView - Tabbar") {
+    AppView(appState: AppState(showTabBar: true))
+}
+#Preview("AppView - Onboarding") {
+    AppView(appState: AppState(showTabBar: false))
 }
