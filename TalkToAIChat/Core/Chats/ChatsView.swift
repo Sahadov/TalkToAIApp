9badecf69a6f697ab07dea0c161654bb3de96d39
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-09-01 21:31:03 -0300

    Add chatModels, chatCell and chatRowBuilder

--- TalkToAIChat/Core/Chats/ChatsView.swift
+++ TalkToAIChat/Core/Chats/ChatsView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ChatsView: View {
+    
+    @State private var chats: [ChatModel] = ChatModel.mocks
+    
     var body: some View {
         NavigationStack {
-            Text("CHATS")
+            List {
+                ForEach(chats) { chat in
+                    ChatRowCellViewBuilder(
+                        currentUserId: nil, // Add cuid
+                        chat: chat,
+                        getAvatar: {
+                            try? await Task.sleep(for: .seconds(1))
+                            return .mock
+                        },
+                        getLastChatMessage: {
+                            try? await Task.sleep(for: .seconds(1))
+                            return .mock
+                        }
+                    )
+                    .anyButton(.highlight, action: {
+                        
+                    })
+                    .removeListRowFormatting()
+                }
+            }
+            .navigationTitle("Chats")
         }
     }
 }
