48ba7f579a8d8c9168403097533293c022225626
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-09-02 11:20:23 -0300

    Add settingsView, ColorHex extension, profileView

---
+++ TalkToAIChat/Core/Settings/SettingsView.swift
@@ -0,0 +1,43 @@
+//
+//  SettingsView.swift
+//  TalkToAIChat
+//
+//  Created by Dmitry Volkov on 02/09/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    @Environment(AppState.self) private var appState
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Button {
+                    onSignOutPressed()
+                } label: {
+                    Text("Sign out")
+                }
+            }
+            .navigationTitle("Settings")
+        }
+    }
+    
+    func onSignOutPressed() {
+        // logic to sign user out of app!
+        
+        dismiss()
+        
+        Task {
+            try? await Task.sleep(for: .seconds(1))
+            appState.updateViewState(showTabBarView: false)
+        }
+    }
+}
+
+#Preview {
+    SettingsView()
+        .environment(AppState())
+}
