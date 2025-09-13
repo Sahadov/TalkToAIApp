7b1adec9f98d77d4101e2c78979032b5f1cb5684
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-09-13 00:06:45 -0300

    Add ChatBubblelViews

---
+++ TalkToAIChat/Core/Chat/ChatView.swift
@@ -0,0 +1,45 @@
+//
+//  ChatView.swift
+//  TalkToAIChat
+//
+//  Created by Dmitry Volkov on 13/09/2025.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    
+    @State private var chatMessages: [ChatMessageModel] = ChatMessageModel.mocks
+    @State private var avatar: AvatarModel? = .mock
+    @State private var currentUser: UserModel? = .mock
+
+    var body: some View {
+        VStack(spacing: 0) {
+            ScrollView {
+                LazyVStack(spacing: 24) {
+                    ForEach(chatMessages) { message in
+                        let isCurrentUser = message.authorId == currentUser?.userId
+                        ChatBubbleViewBuilder(
+                            message: message,
+                            isCurrentUser: isCurrentUser,
+                            imageName: isCurrentUser ? nil : avatar?.profileImageName
+                        )
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(8)
+            }
+            
+            Rectangle()
+                .frame(height: 50)
+        }
+        .navigationTitle(avatar?.name ?? "Chat")
+        .toolbarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        ChatView()
+    }
+}
