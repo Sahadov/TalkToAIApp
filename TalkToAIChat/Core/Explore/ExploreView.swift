a4a57475bf9fe691f263500053543e4d12725042
Dmitry <mrsahadov@gmail.com>
2025-10-06 22:51:51 -0300

    Add explore routing

--- TalkToAIChat/Core/Explore/ExploreView.swift
+++ TalkToAIChat/Core/Explore/ExploreView.swift
@@ -12,15 +12,18 @@ struct ExploreView: View {
     @State private var featuredAvatars: [AvatarModel] = AvatarModel.mocks
     @State private var categories: [CharacterOption] = CharacterOption.allCases
     @State private var popularAvatars: [AvatarModel] = AvatarModel.mocks
+    
+    @State private var path: [NavigationPathOption] = []
 
     var body: some View {
-        NavigationStack {
+        NavigationStack(path: $path) {
             List {
                 featuredSection
                 categorySection
                 popularSection
             }
             .navigationTitle("Explore")
+            .navigationDestinationForCoreModule(path: $path)
         }
     }
     
@@ -34,7 +37,7 @@ struct ExploreView: View {
                         imageName: avatar.profileImageName
                     )
                     .anyButton {
-                        
+                        onAvatarPressed(avatar: avatar)
                     }
                 }
             }
@@ -43,19 +46,22 @@ struct ExploreView: View {
             Text("Featured")
         }
     }
-    
+        
     private var categorySection: some View {
         Section {
             ZStack {
                 ScrollView(.horizontal) {
                     HStack(spacing: 12) {
                         ForEach(categories, id: \.self) { category in
-                            CategoryCellView(
-                                title: category.plural.capitalized,
-                                imageName: Constants.randomImage
-                            )
-                            .anyButton {
-                                
+                            let imageName = popularAvatars.first(where: { $0.characterOption == category })?.profileImageName
+                            if let imageName {
+                                CategoryCellView(
+                                    title: category.plural.capitalized,
+                                    imageName: imageName
+                                )
+                                .anyButton {
+                                    onCategoryPressed(category: category, imageName: imageName)
+                                }
                             }
                         }
                     }
@@ -80,13 +86,21 @@ struct ExploreView: View {
                     subtitle: avatar.characterDescription
                 )
                 .anyButton(.highlight) {
-                    
+                    onAvatarPressed(avatar: avatar)
                 }
                 .removeListRowFormatting()
             }
         } header: {
             Text("Popular")
         }
+    }
+    
+    private func onAvatarPressed(avatar: AvatarModel) {
+        path.append(.chat(avatarId: avatar.avatarId))
+    }
+
+    private func onCategoryPressed(category: CharacterOption, imageName: String) {
+        path.append(.category(category: category, imageName: imageName))
     }
 }
 
