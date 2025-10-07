7caadeb1fb683adc689bd254ab0d5ef84d318c22
Dmitry <mrsahadov@gmail.com>
2025-10-06 21:30:25 -0300

    Add NavigationPathOption. Add routing initializer to CategoryView and ChatView

---
+++ TalkToAIChat/Core/Routing/NavigationPathOption.swift
@@ -0,0 +1,29 @@
+//
+//  NavigationPathOption.swift
+//  TalkToAIChat
+//
+//  Created by Dmitry Volkov on 06/10/2025.
+//
+
+import SwiftUI
+import Foundation
+
+enum NavigationPathOption: Hashable {
+    case chat(avatarId: String)
+    case category(category: CharacterOption, imageName: String)
+}
+
+extension View {
+    
+    func navigationDestinationForCoreModule(path: Binding<[NavigationPathOption]>) -> some View {
+        self
+            .navigationDestination(for: NavigationPathOption.self) { newValue in
+                switch newValue {
+                case .chat(avatarId: let avatarId):
+                    ChatView(avatarId: avatarId)
+                case .category(category: let category, imageName: let imageName):
+                    CategoryListView(path: path, category: category, imageName: imageName)
+                }
+            }
+    }
+}
