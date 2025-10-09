889f241d687fdac874cc0e951306cc04901bf16d
Dmitry <mrsahadov@gmail.com>
2025-10-08 22:16:22 -0300

    Add anonymous sign in functionality

---
+++ TalkToAIChat/Core/AppView/AppView.swift
@@ -0,0 +1,55 @@
+//
+//  AppView.swift
+//  TalkToAIChat
+//
+//  Created by Dmitry Volkov on 14/08/2025.
+//
+
+import SwiftUI
+
+struct AppView: View {
+
+    @Environment(\.authService) private var authService
+    @State var appState: AppState = AppState()
+
+    var body: some View {
+        AppViewBuilder(
+            showTabBar: appState.showTabBar,
+            tabbarView: {
+                TabBarView()
+            },
+            onboardingView: {
+                WelcomeView()
+            }
+        )
+        .environment(appState)
+        .task {
+            await checkUserStatus()
+        }
+    }
+    
+    private func checkUserStatus() async {
+        if let user = authService.getAuthenticatedUser() {
+            // User is authenticated
+            print("User already authenticated: \(user.uid)")
+        } else {
+            // User is not authenticated
+            
+            do {
+                let result = try await authService.signInAnonymously()
+                
+                // log in to app
+                print("Sign in anonymous success: \(result.user.uid)")
+            } catch {
+                print(error)
+            }
+        }
+    }
+}
+
+#Preview("AppView - Tabbar") {
+    AppView(appState: AppState(showTabBar: true))
+}
+#Preview("AppView - Onboarding") {
+    AppView(appState: AppState(showTabBar: false))
+}
