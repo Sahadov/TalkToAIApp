fd03edcb7360797231d6935161a1b14b42cba1c3
Dmitry <mrsahadov@gmail.com>
2025-10-19 22:12:29 -0300

    Add AuthManager

--- TalkToAIChat/Services/Auth/Services/AuthService.swift
+++ TalkToAIChat/Services/Auth/Services/AuthService.swift
@@ -4,18 +4,13 @@
 //
 //  Created by Dmitry Volkov on 10/10/2025.
 //
-
 import SwiftUI
 
-extension EnvironmentValues {
-    @Entry var authService: AuthService = MockAuthService()
-}
-
 protocol AuthService: Sendable {
+    func addAuthenticatedUserListener(onListenerAttached: (any NSObjectProtocol) -> Void) -> AsyncStream<UserAuthInfo?>
     func getAuthenticatedUser() -> UserAuthInfo?
     func signInAnonymously() async throws -> (user: UserAuthInfo, isNewUser: Bool)
     func signInApple() async throws -> (user: UserAuthInfo, isNewUser: Bool)
     func signOut() throws
     func deleteAccount() async throws
 }
-
